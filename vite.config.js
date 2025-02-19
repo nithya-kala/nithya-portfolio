f8bd56744c6b68b3d315635163779b22925b5cbb
nithya <nithya@nithis.in>
2025-02-19 15:00:13 +0530

    added index.js to dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    target: "esnext",
+    rollupOptions: {
+      output: {
+        format: "es",
+        entryFileNames: "index.js", // Ensures Vite outputs an index.js file
+      },
+    },
+  },
 })
